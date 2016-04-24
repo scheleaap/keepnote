d1cdb8a2256d193195fcc93cec98dbb1ef0cc070
Wout <git@wout.maaskant.info>
2016-04-24 14:51:18 +0200

    Added development scripts.

---
+++ run_tests.bat
@@ -0,0 +1,2 @@
+python -m unittest discover -s src -p notes_notemanager_test.py
+::python -m unittest discover -s src -p application_test.py
---
+++ run_tests_allnew.bat
@@ -0,0 +1,2 @@
+python -tt -m unittest discover -s test -p *.py -v
+::python -tt -m unittest discover -s src -p *_test.py -v
